236d472481fd0a2626967bc07ec10b453d955299
boyce <boyce.duan@gmail.com>
2019-01-31 12:55:43 +0800

    新增httpclientpool基础模块

---
+++ sysmodule/HttpClientPoolModule.go
@@ -0,0 +1,19 @@
+package sysmodule
+
+import "github.com/duanhf2012/origin/service"
+
+type HttpClientPoolModule struct {
+	service.BaseModule
+}
+
+func (slf *HttpClientPoolModule) Init(maxpool int) {
+
+}
+
+func (slf *HttpClientPoolModule) SyncRequest() {
+
+}
+
+func (slf *HttpClientPoolModule) Request() {
+
+}
