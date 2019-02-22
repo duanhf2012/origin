034ac53db3e1647601fdf9c439e1499fac0329dc
boyce <boyce.duan@gmail.com>
2019-02-22 18:26:05 +0800

    新增定时器

---
+++ util/Timer.go
@@ -0,0 +1,25 @@
+package util
+
+import "time"
+
+type Timer struct {
+	lasttime     int64
+	timeinterval int64
+}
+
+func (slf *Timer) SetupTimer(ms int32) {
+
+	slf.lasttime = time.Now().UnixNano()
+	slf.timeinterval = int64(ms) * 1e6
+}
+
+func (slf *Timer) CheckTimeOut() bool {
+	now := time.Now().UnixNano()
+	if now-slf.lasttime > slf.timeinterval {
+		slf.lasttime = now
+
+		return true
+	}
+
+	return false
+}
