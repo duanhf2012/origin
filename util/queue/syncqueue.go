de87bd847de611dc5cf664c8d7a8faae0c86514a
boyce <boyce.duan@gmail.com>
2019-03-26 15:03:05 +0800

    新增异步队列

---
+++ util/queue/syncqueue.go
@@ -0,0 +1,51 @@
+package queue
+
+import "sync"
+
+type SyncQueue struct {
+	que   *Queue
+	mutex sync.RWMutex
+}
+
+func (q *SyncQueue) Len() int {
+	q.mutex.RLock()
+	defer q.mutex.RUnlock()
+
+	return q.que.Length()
+}
+
+func (q *SyncQueue) Add(elem interface{}) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+
+	q.que.Add(elem)
+}
+
+func (q *SyncQueue) Peek() interface{} {
+	q.mutex.RLock()
+	defer q.mutex.RUnlock()
+
+	return q.que.Peek()
+}
+
+func (q *SyncQueue) Get(i int) interface{} {
+	q.mutex.RLock()
+	defer q.mutex.RUnlock()
+
+	return q.que.Get(i)
+}
+
+func (q *SyncQueue) Pop() interface{} {
+
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+
+	return q.que.Pop()
+}
+
+func NewSyncQueue() *SyncQueue {
+	syncQueue := SyncQueue{}
+	syncQueue.que = NewQueue()
+
+	return &syncQueue
+}
