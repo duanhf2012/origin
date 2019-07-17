a4aebc01b818227ec2386878e02005d771da57c8
boyce <6549168@qq.com>
2019-07-17 14:58:02 +0800

    新增md5工具库

---
+++ util/md5/md5.go
@@ -0,0 +1,28 @@
+package md5
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+func Md5V(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Md5V2(str string) string {
+	data := []byte(str)
+	has := md5.Sum(data)
+	md5str := fmt.Sprintf("%x", has)
+	return md5str
+}
+
+func Md5V3(str string) string {
+	w := md5.New()
+	io.WriteString(w, str)
+	md5str := fmt.Sprintf("%x", w.Sum(nil))
+	return md5str
+}
