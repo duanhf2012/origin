9623cd27357d023b1808c18c12bef02ba9757b06
boyce <6549168@qq.com>
2019-09-23 14:38:44 +0800

    新增测试模块

---
+++ Test2/Helloworld/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/duanhf2012/origin/cluster"
+	"github.com/duanhf2012/origin/originnode"
+	"github.com/duanhf2012/origin/sysservice"
+)
+
+func main() {
+
+	node := originnode.NewOriginNode()
+	if node == nil {
+		return
+	}
+
+	nodeCfg, _ := cluster.ReadNodeConfig("./config/nodeconfig.json", cluster.GetNodeId())
+	httpserver := sysservice.NewHttpServerService(nodeCfg.HttpPort) // http服务
+	for _, ca := range nodeCfg.CAFile {
+		httpserver.SetHttps(ca.CertFile, ca.KeyFile)
+	}
+	httpserver.SetPrintRequestTime(true)
+
+	node.SetupService(httpserver)
+	node.Init()
+	node.Start()
+}
