93be70877b06aa79cf45dbb7cdd9e604b4b5db21
orgin <boyce.duan@gmail.com>
2022-03-04 10:56:59 +0800

    增大内存池分布

--- network/slicepool.go
+++ network/slicepool.go
@@ -10,38 +10,37 @@ type INetMempool interface {
 }
 
 type memAreaPool struct {
-	minAreaValue int   //最小范围值
-	maxAreaValue int   //最大范围值
-	growthValue int   //内存增长值
-	pool []sync.Pool
+	minAreaValue int //最小范围值
+	maxAreaValue int //最大范围值
+	growthValue  int //内存增长值
+	pool         []sync.Pool
 }
 
-//小于2048时，按64步长增长.>2048时则按2048长度增长
-var memAreaPoolList = [2]*memAreaPool{&memAreaPool{minAreaValue:1,maxAreaValue: 2048,growthValue:64}, &memAreaPool{minAreaValue: 2049,maxAreaValue:65536,growthValue:2048}}
+var memAreaPoolList = [3]*memAreaPool{&memAreaPool{minAreaValue: 1, maxAreaValue: 4096, growthValue: 512}, &memAreaPool{minAreaValue: 4097, maxAreaValue: 40960, growthValue: 4096}, &memAreaPool{minAreaValue: 40961, maxAreaValue: 417792, growthValue: 16384}}
 
-func init(){
-	for i:=0;i<len(memAreaPoolList);i++{
+func init() {
+	for i := 0; i < len(memAreaPoolList); i++ {
 		memAreaPoolList[i].makePool()
 	}
 }
 
-func NewMemAreaPool() *memAreaPool{
+func NewMemAreaPool() *memAreaPool {
 	return &memAreaPool{}
 }
 
-func (areaPool *memAreaPool) makePool(){
-	poolLen := (areaPool.maxAreaValue - areaPool.minAreaValue+1)/areaPool.growthValue
-	areaPool.pool = make([]sync.Pool,poolLen)
-	for i:=0;i<poolLen;i++{
-		memSize := (areaPool.minAreaValue-1)+(i+1)*areaPool.growthValue
-		areaPool.pool[i] = sync.Pool{New: func() interface{}{
+func (areaPool *memAreaPool) makePool() {
+	poolLen := (areaPool.maxAreaValue - areaPool.minAreaValue + 1) / areaPool.growthValue
+	areaPool.pool = make([]sync.Pool, poolLen)
+	for i := 0; i < poolLen; i++ {
+		memSize := (areaPool.minAreaValue - 1) + (i+1)*areaPool.growthValue
+		areaPool.pool[i] = sync.Pool{New: func() interface{} {
 			//fmt.Println("make memsize:",memSize)
-			return make([]byte,memSize)
+			return make([]byte, memSize)
 		}}
 	}
 }
 
-func (areaPool *memAreaPool) makeByteSlice(size int) []byte{
+func (areaPool *memAreaPool) makeByteSlice(size int) []byte {
 	pos := areaPool.getPosByteSize(size)
 	if pos > len(areaPool.pool) || pos == -1 {
 		return nil
@@ -50,8 +49,8 @@ func (areaPool *memAreaPool) makeByteSlice(size int) []byte{
 	return areaPool.pool[pos].Get().([]byte)[:size]
 }
 
-func (areaPool *memAreaPool) getPosByteSize(size int) int{
-	pos := (size - areaPool.minAreaValue)/areaPool.growthValue
+func (areaPool *memAreaPool) getPosByteSize(size int) int {
+	pos := (size - areaPool.minAreaValue) / areaPool.growthValue
 	if pos >= len(areaPool.pool) {
 		return -1
 	}
@@ -59,29 +58,29 @@ func (areaPool *memAreaPool) getPosByteSize(size int) int{
 	return pos
 }
 
-func (areaPool *memAreaPool) releaseByteSlice(byteBuff []byte) bool{
+func (areaPool *memAreaPool) releaseByteSlice(byteBuff []byte) bool {
 	pos := areaPool.getPosByteSize(cap(byteBuff))
-	if pos > len(areaPool.pool) || pos == -1{
+	if pos > len(areaPool.pool) || pos == -1 {
 		panic("assert!")
 		return false
 	}
-	
+
 	areaPool.pool[pos].Put(byteBuff)
 	return true
 }
 
-func (areaPool *memAreaPool) MakeByteSlice(size int) []byte{
-	for i:=0;i<len(memAreaPoolList);i++{
+func (areaPool *memAreaPool) MakeByteSlice(size int) []byte {
+	for i := 0; i < len(memAreaPoolList); i++ {
 		if size <= memAreaPoolList[i].maxAreaValue {
 			return memAreaPoolList[i].makeByteSlice(size)
 		}
 	}
 
-	return nil
+	return make([]byte, size)
 }
 
 func (areaPool *memAreaPool) ReleaseByteSlice(byteBuff []byte) bool {
-	for i:=0;i<len(memAreaPoolList);i++{
+	for i := 0; i < len(memAreaPoolList); i++ {
 		if cap(byteBuff) <= memAreaPoolList[i].maxAreaValue {
 			return memAreaPoolList[i].releaseByteSlice(byteBuff)
 		}
