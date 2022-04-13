18d620118eafe7a50e7cbe5a6a5c4078c74fde96
orgin <boyce.duan@gmail.com>
2022-04-13 11:47:13 +0800

    在help中新增编译参数信息

--- util/buildtime/build.go
+++ util/buildtime/build.go
@@ -1,5 +1,12 @@
 package buildtime
 
+/*
+//查询buildtime包中的位置，在github.com/duanhf2012/origin/util/buildtime.BuildTime中
+go tool nm ./originserver.exe |grep buildtime
+
+//编译传入编译时间信息
+go build -ldflags "-X 'github.com/duanhf2012/origin/util/buildtime.BuildTime=20200101'"
+*/
 var BuildTime string
 
 
