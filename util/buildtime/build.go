5a6a4c8a0d92984b060dc0f865d69401b6108dbb
orgin <boyce.duan@gmail.com>
2022-09-22 10:37:27 +0800

    新增编译标记以及编译操作系统类型

--- util/buildtime/build.go
+++ util/buildtime/build.go
@@ -6,10 +6,15 @@ go tool nm ./originserver.exe |grep buildtime
 
 //编译传入编译时间信息
 go build -ldflags "-X 'github.com/duanhf2012/origin/util/buildtime.BuildTime=20200101'"
+go build -ldflags "-X github.com/duanhf2012/origin/util/buildtime.BuildTime=20200101 -X github.com/duanhf2012/origin/util/buildtime.BuildTag=debug"
 */
 var BuildTime string
-
+var BuildTag  string
 
 func GetBuildDateTime() string {
 	return BuildTime
+}
+
+func GetBuildTag() string {
+	return BuildTag
 }
