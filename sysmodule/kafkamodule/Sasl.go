4d35a7b98bc677f81a3aa709a9b10c61b0c20886
boyce <6549168@qq.com>
2026-07-22 21:12:21 +0800

    chore: initialize clean v3 baseline

--- sysmodule/kafkamodule/Sasl.go
+++
@@ -1,7 +0,0 @@
-package kafkamodule
-
-type Sasl struct {
-	UserName   string `json:"UserName"`
-	Passwd     string `json:"Passwd"`
-	InstanceId string `json:"InstanceId"`
-}
--- util/hash/hash.go
+++
@@ -1,7 +0,0 @@
-package hash
-
-import "hash/crc32"
-
-func HashNumber(s string) uint {
-	return uint(crc32.ChecksumIEEE([]byte(s)))
-}
